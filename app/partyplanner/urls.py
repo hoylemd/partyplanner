54713cab81f05b15bc218088d2aba10cf580279a
Michael D. Hoyle <hoylemd@gmail.com>
2019-08-28 13:09:49 -0400

    Add login/logout

--- app/partyplanner/urls.py
+++ app/partyplanner/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from partyplanner.views import Login, Logout
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('events/', include('events.urls')),
+    path('', Login.as_view(), name='login'),
+    path('logout/', Logout.as_view(), name='logout'),
 ]
---
+++ app/partyplanner/views.py
@@ -0,0 +1,34 @@
+"""
+Modified from:
+https://coderwall.com/p/sll1kw/django-auth-class-based-views-login-and-logout
+"""
+
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.views.generic import FormView, RedirectView
+from django.urls import reverse_lazy
+
+
+class Login(FormView):
+    """
+    Provides the ability to login as a user with a username and password
+    """
+    success_url = reverse_lazy('event_list')
+    form_class = AuthenticationForm
+    template_name = 'login.html'
+
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+
+        return super().form_valid(form)
+
+
+class Logout(RedirectView):
+    """
+    Provides users the ability to logout
+    """
+    url = reverse_lazy('login')
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return super().get(request, *args, **kwargs)
