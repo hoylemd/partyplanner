12b1ac78dd28bf12d286aad0c9ca588d086491a8
Michael D. Hoyle <hoylemd@gmail.com>
2019-10-11 12:15:18 -0400

    build the first event test

--- app/events/tests/test_event_views.py
+++ app/events/tests/test_event_views.py
@@ -1,12 +1,27 @@
-from partyplanner.tests.utils import JSONTestCase
+from django.contrib.auth import get_user_model
+
+from partyplanner.tests.utils import JSONTestCase, make_token
+
+User = get_user_model()
 
 
 class TestEventViews(JSONTestCase):
-    fixtures = ['events.json']
+    fixtures = ['users.json', 'events.json']
 
     def test_get_event(self):
         """Should return 200 with the event data"""
-        pass
+        user = User.objects.get(pk=70001)  # test user
+        token = make_token(user)
+        headers = {'HTTP_AUTHORIZATION': f'JWT {token}'}
+
+        resp = self.client.get('/api/events/80001/', **headers)
+
+        self.assertContainsJSON(resp, {
+            'pk': 80001,
+            'name': 'Test Event',
+            'image': 'http://example.com/event-image.png',
+            'starts_at': '2019-10-15T13:00:00Z'  # != fixture? normalized!
+        })
 
     def test_get_event__notfound(self):
         """Should return 404"""
