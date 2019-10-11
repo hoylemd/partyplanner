46e5f3c883e44296146896d53ce65a0b9559e31e
Michael D. Hoyle <hoylemd@gmail.com>
2019-10-11 12:01:43 -0400

    add fixture and stub out tests for events

--- app/events/tests/test_event_views.py
+++ app/events/tests/test_event_views.py
@@ -2,4 +2,16 @@ from partyplanner.tests.utils import JSONTestCase
 
 
 class TestEventViews(JSONTestCase):
-    pass
+    fixtures = ['events.json']
+
+    def test_get_event(self):
+        """Should return 200 with the event data"""
+        pass
+
+    def test_get_event__notfound(self):
+        """Should return 404"""
+        pass
+
+    def test_get_event__unauth(self):
+        """Should return 401"""
+        pass
