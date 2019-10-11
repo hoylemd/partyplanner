60a30c2dae997ff0c9404ad6b100c6b6b92807af
Michael D. Hoyle <hoylemd@gmail.com>
2019-10-11 15:34:17 -0400

    Handle an edge case with ends_at validation

--- app/events/serializers.py
+++ app/events/serializers.py
@@ -9,9 +9,19 @@ ISO_8601_FORMAT = r'%Y-%m-%d %H:%M:%S'
 
 class EventSerializer(serializers.ModelSerializer):
     def validate_ends_at(self, value):
-        start_time_raw = self.initial_data['starts_at']
-        start_time = datetime.strptime(start_time_raw, ISO_8601_FORMAT)
-        start_time = start_time.astimezone()  # make it UTC tz-aware
+        try:
+            start_time_raw = self.initial_data['starts_at']
+            start_time = datetime.strptime(start_time_raw, ISO_8601_FORMAT)
+            start_time = start_time.astimezone()  # make it UTC tz-aware
+        except KeyError:
+            # starts_at not submitted, check the instance
+            try:
+                start_time = self.instance.starts_at
+            except AttributeError:
+                # instance doesn't exist, this is a create
+                # just pass it - starts_at validation will catch the error
+                return value
+
         if value < start_time:
             raise serializers.ValidationError('End time before start time.')
 
